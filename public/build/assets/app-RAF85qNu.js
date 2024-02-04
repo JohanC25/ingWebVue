9cd672173b5dd1274757b792306494eb6267171e
JohanC25 <johanmcm@hotmail.com>
2024-02-04 13:21:44 -0500

    subir

---
+++ public/build/assets/app-RAF85qNu.js
@@ -0,0 +1,1 @@
+require("./bootstrap");var r=require("vue"),u=t(require("./components/ComponentA.vue"));function t(e){return e&&e.__esModule?e:{default:e}}(0,r.createApp)(u.default).mount("#componente-a");
--- vite.config.js
+++ vite.config.js
@@ -13,4 +13,10 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    resolve: {
+        alias: {
+            '@': '/resources/js',
+            'vue': 'vue/dist/vue.esm-bundler.js'
+        },
+    },
 });
